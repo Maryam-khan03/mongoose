db814421456c8d82a25df46409a4ab51184ff7f2
Maryam <maryamKhannadeem03@gmail.com>
2024-11-18 07:32:40 +0500

    Add files via upload

--- book.js
+++ book.js
@@ -1,8 +1,42 @@
-const express = require('express')
-const router = express.Router()
-const controller = require('../controller/book')
-router.post('/book',controller.addBook)
-router.get('/book/:id',controller.getSingleBook)
-router.put('/book/update/:id',controller.updateBook)
-router.delete('/book/delete/:id',controller.deleteBook)
-module.exports = router
+const Book = require('../models/.book');
+const addBook = async(req, res)=>
+{
+    const data = req.body;
+    const object = await Book.create(data);
+    res.json({"message":"Object created successfully", object})
+}
+const getSingleBook = async(req,res)=>
+{
+    const id = req.params.id;
+    const object = await Book.findByID({_id:id});
+    res.json({"message":"Object found successfully", object})
+}
+const updateBook = async(req,res)=>
+{
+    const id = req.params.id;
+    const object = req.body;
+    const updateBook = await Book.findByIDAndUpdate(id,object,{new: true});
+    if(updateBook === null)
+    {
+        return res.status(404).send("Book not found");
+    }
+    res.status(200).send("Book updated");
+}
+const deleteBook = async(req,res)=>
+{
+    const id = req.params.id;
+    const deleteBook = await Book.findByIDAndDelete(id);
+    if(deleteBook === null)
+        {
+            return console.log("deleted")
+        }
+        res.json({"message":"Book deleted", deletebook});
+        
+}
+module.exports = 
+{
+    addBook,
+    getSingleBook,
+    updateBook,
+    deleteBook
+}
